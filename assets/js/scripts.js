6161e2cd2bf995f08ea4dacd280a67b9af35429d
Moradi1412 <moradi.javid@gmail.com>
2021-08-01 00:05:19 -0700

    questions without fetech

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,141 @@
 //DOM Element Declaration
 var card = $(".inner");
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-//Variable Declarations
-var amount = 10;
+fetch("https://opentdb.com/api.php?amount=10&category=20&difficulty=medium&type=multiple")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (response) {
+        console.log(response.results);
+        for (let index = 0; index < 10; index++) {
+        var answers = [response.results[index].correct_answer + "," + response.results[index].incorrect_answers]; 
 
-//Code
-fetch(`https://opentdb.com/api.php?amount=${amount}&category=20&difficulty=easy&type=multiple`)
-.then(response => response.json()
-.then(data => console.log(data)));
+        console.log(response.results[index].question)
+        console.log (answers);
+        console.log (response.results[index].correct_answer)
+        }})
 
-alert($(window).width());
+
+const questions = [
+    {
+      question: 'What is 2 + 2?',
+      answers: [
+        { text: '4', correct: true },
+        { text: '22', correct: false }
+      ]
+    },
+    {
+      question: 'Who is the best YouTuber?',
+      answers: [
+        { text: 'Web Dev Simplified', correct: true },
+        { text: 'Traversy Media', correct: true },
+        { text: 'Dev Ed', correct: true },
+        { text: 'Fun Fun Function', correct: true }
+      ]
+    },
+    {
+      question: 'Is web development fun?',
+      answers: [
+        { text: 'Kinda', correct: false },
+        { text: 'YES!!!', correct: true },
+        { text: 'Um no', correct: false },
+        { text: 'IDK', correct: false }
+      ]
+    },
+    {
+      question: 'What is 4 * 2?',
+      answers: [
+        { text: '6', correct: false },
+        { text: '8', correct: true }
+      ]
+    }
+  ]
+
+  console.log(questions)
+
+// //Variable Declarations
+// var amount = 10;
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions
+  //.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+
+// alert($(window).width());
 
 //DOM Event Listeners
 card.click(() => card.toggleClass("is-flipped__Y"));
